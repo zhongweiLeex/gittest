6a1bbf2cc9fac11ebbead107f845789091c42aa3
lizhongwei <lzw1003362793@Gmail.com>
2022-04-06 20:40:29 +0800

    this is a GitTest

---
+++ src/main/java/com/lzw/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lzw.git;
+
+/**
+ * @author ZhongweiLeex
+ * @Date 2022-04-06 20:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
